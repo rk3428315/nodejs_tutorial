e6cc8f16a5ceb40a547997ce33439db2a34cc1e0
Ravi <ravikumar08979@gmail.com>
2023-10-18 16:47:56 +0530

    use of middleware and haddling req,res

---
+++ 11.MiddleWare/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const reqFilter = (req, res, next)=>{
+    if(!req.query.age){
+     res.send("Please provide age!")   
+    }else if(req.query.age < 18){
+        res.send("Age is less than 18.")
+    }
+    else{
+
+        next();
+    }
+}
+
+app.use(reqFilter);
+
+// To render the home page
+app.get('/',(req,res)=>{
+    res.send("Welcome to Home page");
+});
+
+// To render the user page
+app.get('/user',(req,res)=>{
+    res.send("Welcome to user page");
+});
+
+app.listen(5000);
